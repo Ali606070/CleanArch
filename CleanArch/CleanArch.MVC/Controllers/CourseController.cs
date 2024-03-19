e7c5db6b613b3fe32443cbec96d02ee61bf0f4cc
ali asswad <aliasswadali@gmail.com>
2024-03-19 11:16:55 -0700

    changes for presentages , controler courses

---
+++ CleanArch/CleanArch.MVC/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService courseService;
+
+        public CourseController(ICourseService  courseService)
+        {
+            this.courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CoursViewModel coursViewModel = courseService.GetCourses();
+
+            return View(coursViewModel);
+        }
+    }
+}
